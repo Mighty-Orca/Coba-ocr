1f40a811c370796aa11899658ebf0ebd391a7e83
Mighty-Orca <mfazakhalid@gmail.com>
2025-06-29 20:26:24 +0700

    Update process.py

--- ocr/process.py
+++ ocr/process.py
@@ -10,7 +10,7 @@ if platform.system() == "Windows":
 def extract_text_from_image(image_path):
     try:
         image = Image.open(image_path)
-        text = pytesseract.image_to_string(image, lang='ind')  # pakai 'eng' kalau hasil masih aneh
+        text = pytesseract.image_to_string(image)  # pakai 'eng' kalau hasil masih aneh
         return text
     except Exception as e:
         return f"Terjadi error saat proses OCR: {e}"
